4e8c74046d298bb48627dc2ac2243e2b58a9e8ca
Rohan Shah <sha351@csiro.au>
2017-11-19 16:04:55 +1000

    Minor changes

--- R/runHeuristics.R
+++ R/runHeuristics.R
@@ -41,7 +41,7 @@
 #' result <- fitClusterModel(data, startingPoints, n.iter = 200, D_hom = diag(2)*4, V_hom = cbind(c(0.005, 0), c(0, 0.1))/3, n_hom = 30, D_err = diag(2), V_err = diag(2)*10/3, n_err = 300, V_het = diag(2)*0.025/3, n_het = 1500)
 #' plot(result, chainIndex = 1)
 #' heuristicResults <- runHeuristics(result, minHomozygoteSize = 200)
-#' plot(heuristicResults, chainIndex = heuristicResult$chainIndex)
+#' plot(heuristicResults, chainIndex = heuristicResults$chainIndex)
 runHeuristics <- function(fittedModel, minHomozygoteSize)
 {
   N <- nrow(fittedModel$data)
